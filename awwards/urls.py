fda0bc041231a3c8e613d5aabd398bcdfee1308c
andyjohn23 <andrewnick906@gmail.com>
2020-11-28 15:33:20 +0300

    created the footerand styled it

--- awwards/urls.py
+++ awwards/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('awwards_users.urls'))
+    path('', include('awwards_users.urls')),
+    
 ]
