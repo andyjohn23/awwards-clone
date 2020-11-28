14668f208447c8168992dad18a912600066c83d4
andyjohn23 <andrewnick906@gmail.com>
2020-11-28 15:58:56 +0300

    created the registration, login models, forms and views

--- awwards_users/views.py
+++ awwards_users/views.py
@@ -1,6 +1,106 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+from django.contrib.auth import login, logout, authenticate
+from .forms import RegisterUserForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.contrib import messages
+from .models import UserAccount
+
 
 # Create your views here.
 
+
 def index(request):
-    return render(request, 'awwards-users/index.html')
+
+    context = {}
+
+    user = request.user
+    if user.is_authenticated:
+        return redirect('profile')
+
+    destination = get_redirect_if_exists(request)
+    if request.POST:
+        form = AuthenticationForm(request.POST)
+        if form.is_valid():
+            email = request.POST['email']
+            password = request.POST['password']
+            user = authenticate(email=email, password=password)
+            if user:
+                login(request, user)
+                destination = get_redirect_if_exists(request)
+                if destination:
+                    return redirect(destination)
+                return redirect('index')
+
+        else:
+            context['login_form'] = form
+
+    return render(request, 'instausers/index.html', context)
+
+
+def register(request, *arg, **kwargs):
+    user = request.user
+
+    if user.is_authenticated:
+        return HttpResponse('You are already authenticated as { user.email }.')
+    context = {}
+
+    if request.POST:
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data.get('email').lower()
+            raw_password = form.cleaned_data.get('password1')
+            account = authenticate(email=email, password=raw_password)
+            login(request, account)
+            destination = get_redirect_if_exists(request)
+            if destination:
+                return redirect(destination)
+            return redirect('profile')
+        else:
+            context['register_form'] = form
+
+    return render(request, 'instausers/register.html', context)
+
+
+def logout_user(request, *args, **kwargs):
+    logout(request)
+    return redirect("index")
+
+
+def login_user(request, *args, **kwargs):
+
+    context = {}
+
+    user = request.user
+    if user.is_authenticated:
+        return redirect('profile')
+
+    destination = get_redirect_if_exists(request)
+    if request.POST:
+        form = AuthenticationForm(request.POST)
+        if form.is_valid():
+            email = request.POST['email']
+            password = request.POST['password']
+            user = authenticate(email=email, password=password)
+            if user:
+                login(request, user)
+                destination = get_redirect_if_exists(request)
+                if destination:
+                    return redirect(destination)
+                return redirect('index')
+
+        else:
+            context['login_form'] = form
+
+    return render(request, 'instausers/login.html', context)
+
+
+def get_redirect_if_exists(request):
+    redirect = None
+    if request.GET:
+        if request.GET.get('next'):
+            redirect = str(request.GET.get('next'))
+
+    return redirect
